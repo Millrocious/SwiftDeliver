ca077a3ccccfb0f8e7e48b85d219a1447bdb9336
Millrocious <79750098+Millrocious@users.noreply.github.com>
2026-02-21 20:17:40 +0200

    Add Serilog configuration with correlationId

--- src/SwiftDeliver.ApiGateway/Program.cs
+++ src/SwiftDeliver.ApiGateway/Program.cs
@@ -1,5 +1,15 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) => 
+    config.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.MapGet("/", () => "Hello World!");
 
